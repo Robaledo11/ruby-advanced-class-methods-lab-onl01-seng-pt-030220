fb9b71cad8e29719a509abcf5f5d3e03dea8a435
ROBERT  ALEDO <robertaledo22@gmail.com>
2020-04-02 06:10:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,42 +6,38 @@ class Song
     @@all
   end
 
-  def save
-    self.class.all << self
+  def self.create
+    song = self.new
+    self.all << song
+    song
   end
 
-def self.create
+  def self.new_by_name(name)
     song = self.new
-    song.save
+    song.name = name
     song
-  end 
+  end
 
-def self.new_by_name(name)
-  song = self.new 
-  song.name = name 
-  song 
-end
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    self.all << song
+    song
+  end
 
-def self.create_by_name(name)
-  song = self.create 
-  song.name = name
-  song 
-end
+  def self.find_by_name(name)
+    @@all.detect {|song| song.name == name}
+  end
 
-def self.find_by_name(name)
-    @@all.find {|song| song.name == name} 
-  end 
-
-def self.find_or_create_by_name(name)
+  def self.find_or_create_by_name(name)
     if self.find_by_name(name)
       self.find_by_name(name)
     else
       self.create_by_name(name)
     end
   end
-end
 
-ddef self.alphabetical
+  def self.alphabetical
     self.all.sort_by! {|song| song.name}
   end
 
