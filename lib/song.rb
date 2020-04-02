1ce865d11515c3994ad1667df67cacefd465db83
ROBERT  ALEDO <robertaledo22@gmail.com>
2020-04-02 05:54:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,8 +33,8 @@ def self.find_by_name(name)
       if song.name == name
         return song
       end 
-
-def.self.find_or_create_by_name(name)
- self.find_by_name(name) || self.create_by_name(name)
-end
+      
+def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
 
